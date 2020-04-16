18e259a6561b0823d3af306339ef2f1df96a8d95
Philipp Schleich <schleich.philipp@rwth-aachen.de>
2020-04-16 16:47:29 +0200

    removed some potentially problem-causing lines

--- src/setup.py
+++ src/setup.py
@@ -4,12 +4,7 @@ import os
 setuptools.setup(
     name="qulacs-test",
     version="0.1.0",
-    package_dir={'' : 'python'},
     py_modules=['qulacstest'],
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ),
     install_requires=[
         'z-quantum-core',
         'cmake',
