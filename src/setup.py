a257803ebd716111a50c67074dd31f40b598fa2a
Philipp Schleich <schleich.philipp@rwth-aachen.de>
2020-04-16 17:45:19 +0200

    still testing z-quantum-core, maybe not working...

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="0.1.0",
     py_modules=['qulacstest'],
     install_requires=[
-        'z-quantum-core',
+      #  'z-quantum-core',
         'cmake',
         'gcc7',
         'qulacs'
